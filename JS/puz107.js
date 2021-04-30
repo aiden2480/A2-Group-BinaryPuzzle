a4562f88787e93a389b7d5ca53545f16ca954494
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2021-04-30 21:10:45 +1000

    Update CSS style and index page formatting

--- JS/puz107.js
+++ JS/puz107.js
@@ -33,7 +33,7 @@ function loadGrid(g) { // 21/4/21 Aiden - Fills in the grid from a template
     }
 
     console.log(grid);
-    document.getElementById("arrayView1D").value = grid;
-    document.getElementById("arrayView2D").value = concatenation;
+    document.getElementById("arrayView1D").value = `[${grid}]`;
+    document.getElementById("arrayView2D").value = concatenation.trim();
     document.getElementById("arrayViewCell").value = grid[3][2]; // Row 4, Column 3
 }
